a9ece1b584bc638d915fa1af7ec4cd7ee9a23923
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-07-22 12:26:22 -0500

    more work on the basic types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,4 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to the [Haskell Package Versioning Policy](https://pvp.haskell.org/).
 
-## [0.1.0] - 2018-07-18
-
-### Added
-- Initial release
+## Unreleased
--- src/Termbox/Banana.hs
+++ src/Termbox/Banana.hs
@@ -1,28 +1,69 @@
-{-# language LambdaCase          #-}
-{-# language ScopedTypeVariables #-}
-{-# language TypeFamilies        #-}
+{-# language DerivingStrategies         #-}
+{-# language GeneralizedNewtypeDeriving #-}
+{-# language LambdaCase                 #-}
+{-# language ScopedTypeVariables        #-}
+{-# language TypeFamilies               #-}
 
 module Termbox.Banana
   ( TermboxEvent
   , main
+  , Scene
+  , set
+  , done
+    -- * Re-exports
+  , Termbox.Cell(..)
+  , Termbox.Event(..)
+  , Termbox.InitError(..)
+  , Termbox.Key(..)
   ) where
 
-import Control.Concurrent.Async
 import Control.Concurrent.STM
-import Control.Monad
-import Data.Void
+import Data.Function (fix)
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 
 import qualified Termbox
 
+-- | A @termbox@ event. This type alias exists only for Haddock readability;
+-- in code, you are encouraged to use
+--
+-- * @Event@ for @reactive-banana@ events
+-- * @Termbox.Event@ for @termbox@ events
 type TermboxEvent
   = Termbox.Event
+
+-- | The scene to render.
+data Scene a
+  = SceneDone a
+  | SceneDraw (IO ())
+
+instance Monoid (Scene a) where
+  mempty = SceneDraw mempty
+  mappend = (<>)
+
+instance Semigroup (Scene a) where
+  SceneDone x <> _ = SceneDone x
+  _ <> SceneDone x = SceneDone x
+  SceneDraw x <> SceneDraw y = SceneDraw (x <> y)
+
+done :: a -> Scene a
+done =
+  SceneDone
+
+-- | Set a cell's value.
+set :: (col ~ Int, row ~ Int) => col -> row -> Termbox.Cell -> Scene a
+set x y z =
+  SceneDraw (Termbox.set x y z)
 
 type EventSource a
   = (AddHandler a, a -> IO ())
 
-main :: (Event TermboxEvent -> MomentIO (Behavior (Either a (IO ())))) -> IO a
+main
+  :: (width ~ Int, height ~ Int)
+  => (  Event TermboxEvent
+     -> Behavior (width, height)
+     -> MomentIO (Behavior (Scene a)))
+  -> IO a
 main run =
   Termbox.main $ do
     doneVar :: TMVar a <-
@@ -36,46 +77,53 @@ main run =
         eEvent :: Event TermboxEvent <-
           fromAddHandler eventAddHandler
 
-        moment run eEvent (atomically . putTMVar doneVar)
+        let
+          eResize :: Event (Int, Int)
+          eResize =
+            filterJust
+              ((\case
+                Termbox.EventResize w h -> Just (w, h)
+                _ -> Nothing)
+              <$> eEvent)
+
+        bSize :: Behavior (Int, Int) <- do
+          flip stepper eResize =<<
+            liftIO Termbox.size
+
+        moment run eEvent bSize (atomically . putTMVar doneVar)
+
     actuate network
 
-    let
-      thread1 :: IO Void
-      thread1 =
-        forever (Termbox.poll >>= fireEvent)
-
-    let
-      thread2 :: IO a
-      thread2 =
-        atomically (readTMVar doneVar)
-
-    race thread1 thread2 >>=
-      either absurd pure
+    fix $ \loop -> do
+      Termbox.poll >>= fireEvent
+      atomically (tryReadTMVar doneVar) >>=
+        maybe loop pure
 
 moment
-  :: (Event TermboxEvent -> MomentIO (Behavior (Either a (IO ()))))
+  :: (width ~ Int, height ~ Int)
+  => (  Event TermboxEvent
+     -> Behavior (width, height)
+     -> MomentIO (Behavior (Scene a)))
   -> Event TermboxEvent
+  -> Behavior (width, height)
   -> (a -> IO ())
   -> MomentIO ()
-moment run eEvent done = do
-  bWrite :: Behavior (Either a (IO ())) <-
-    run eEvent
+moment run eEvent bSize abort = do
+  bScene :: Behavior (Scene a) <-
+    run eEvent bSize
 
-  eWrite :: Event (Future (Either a (IO ()))) <-
-    changes bWrite
+  eScene :: Event (Future (Scene a)) <-
+    changes bScene
 
   let
-    render :: IO () -> IO ()
-    render act =
-      Termbox.clear mempty mempty *> act <* Termbox.flush
+    render :: Scene a -> IO ()
+    render = \case
+      SceneDone x ->
+        abort x
+      SceneDraw m -> do
+        Termbox.clear mempty mempty
+        m
+        Termbox.flush
 
-  (`whenRight` liftIO . render) =<<
-    valueB bWrite
-
-  reactimate' ((fmap.fmap) (either done render) eWrite)
-
-whenRight :: Monad m => Either a b -> (b -> m ()) -> m ()
-whenRight x f =
-  case x of
-    Left _ -> pure ()
-    Right y -> f y
+  liftIO . render =<< valueB bScene
+  reactimate' ((fmap.fmap) render eScene)
